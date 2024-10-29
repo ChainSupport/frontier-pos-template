11cc805b1f803d3bf3a4bd2dcff335589ae8b8b1
weimeme <weimeme067@gmail.com>
2024-10-29 19:00:05 +0800

    update ExistentialDeposit to NON-ZERO

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -708,7 +708,7 @@ impl pallet_indices::Config for Runtime {
 }
 
 parameter_types! {
-    pub const ExistentialDeposit: Balance = 0 * DOLLARS;
+    pub const ExistentialDeposit: Balance = DOLLARS / 1000;
     // For weight estimation, we assume that the most locks on an individual account will be 50.
     // This number may need to be adjusted in the future if this assumption no longer holds true.
     pub const MaxLocks: u32 = 50;
