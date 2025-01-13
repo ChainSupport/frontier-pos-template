217cb08b2884e244f747880278491b958edf30a0
weimeme <weimeme067@gmail.com>
2025-01-13 11:03:49 +0000

    update

--- scripts/generate_session_keys.sh
+++ scripts/generate_session_keys.sh
@@ -2,12 +2,12 @@
 
 ../target/release/substrate key inspect --output-type json --scheme ed25519 "//$SESSION_KEYS_PASSWORD"//fir//ed//$INDEX
 
-../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD"//fir//ed//$INDEX
+../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD/fir/sr/$INDEX"
 
-../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD"//fir//ed//$INDEX
+../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD/fir/sr/$INDEX"
 
-../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD"//fir//ed//$INDEX
+../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD/fir/sr/$INDEX"
 
-../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD"//fir//ed//$INDEX
+../target/release/substrate key inspect --output-type json --scheme sr25519 "//$SESSION_KEYS_PASSWORD/fir/sr/$INDEX"
 
 ../target/release/substrate key inspect --output-type json --scheme ecdsa "//$SESSION_KEYS_PASSWORD"//fir//ed//$INDEX
